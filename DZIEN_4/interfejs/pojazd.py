4cba8ab145a791b8ca84ce88c4649700560e59bd
albim72 <marcin.albiniak72@gmail.com>
2025-02-13 11:18:43 +0100

    Update pojazd.py

--- DZIEN_4/interfejs/pojazd.py
+++ DZIEN_4/interfejs/pojazd.py
@@ -5,7 +5,13 @@ class Pojazd(IPojazd):
         self.marka = marka
         self.model = model
         self.rodzaj_silnika = rodzaj_silnika
-        
+
     def __repr__(self):
         return f'samochód {self.marka} {self.model}\nrodzaj silnika -> {self.rodzaj_silnika}'
-    
+
+    def spalanie(self, odl, jedn):
+        return jedn*100/odl
+
+    def kosztyprzejazdu(self, odl, jedn, cenal):
+        return jedn*cenal
+        # return self.spalanie(odl,jedn)*(odl/100)*cenal
