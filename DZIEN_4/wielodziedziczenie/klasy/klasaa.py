4be065f989de0ea5c089cfd402a97d6e2ba8929c
albim72 <marcin.albiniak72@gmail.com>
2025-02-13 11:26:53 +0100

    Create klasaa.py

---
+++ DZIEN_4/wielodziedziczenie/klasy/klasaa.py
@@ -0,0 +1,10 @@
+class KlasaA:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        
+    def suma(self):
+        return self.x + self.y
+    
+    def multi(self):
+        return self.x*self.y
